446aef96981987770cfb850d76124242b3ba8776
Pawel <pawmag.code@gmail.com>
2025-03-20 21:13:01 +0100

    Apply state to movie card component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,42 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     { id: 1, title: "Terminator", release_date: "2020" },
     { id: 2, title: "John Wick", release_date: "20201520" },
     { id: 3, title: "Matrix", release_date: "2000" },
   ];
 
-  const handleSearch = () => {
-
-  }
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+  };
 
   return (
     <div className="home">
-        <form onSubmit={handleSearch} className="search-form">
-            <input 
-            type="text" 
-            placeholder="Search for movies..." 
-            className="search-input" 
-            />
-            <button type="submit" className="search-button">Search</button>
-        </form>
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
       <div className="mocies-grid">
-        {movies.map(movie => ( 
-            <MovieCard movie={movie} key={movie.id} /> 
+        {movies.map((movie) => 
+          movie.title.toLowerCase().startsWith(searchQuery) && (
+          <MovieCard movie={movie} key={movie.id} />
         ))}
       </div>
     </div>
   );
 }
 
-export default Home
+export default Home;
